ab96d7eecfddefc032837a5f826d4abe2e3496f4
JaeHyunGround <gothddlek@naver.com>
2023-08-02 02:19:23 +0900

    Feat : CategoryPage 구현 완료
    CategoryPage 구현 완료.
    아이콘과 페이지 이름, 뒤로가기 버튼이 있는 SubHeader도 제작하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainPage from "./pages/Main/MainPage";
 import LoginPage from "./pages/Account/LoginPage";
+import CategoryPage from "./pages/Category/CategoryPage";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/category" element={<CategoryPage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/Category/CategoryPage.jsx
+++ src/pages/Category/CategoryPage.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import "../../styles/Category/CategoryPage.css"
+import SubHeader from '../../components/SubHeader';
+import CategoryList from '../../components/Category/CategoryList';
 
 const CategoryPage = () => {
+
+    const [category, setCategory] = useState({
+        "categories": [
+          {
+            "id": 1,
+            "name": "제철 과일"
+          },
+          {
+            "id": 2,
+            "name": "농산물"
+          },
+          // ... 추가적인 1차 카테고리 정보 ...
+        ]
+      })
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <SubHeader page="카테고리" />
+            <div className='category_container'>
+                {category.categories.map((list, idx) => <CategoryList list={list} key={idx} />)}
+            </div>
+        </>
     );
 };
 
