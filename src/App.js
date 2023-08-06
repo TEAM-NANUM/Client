06232896b6eb72e0a820aba73e1185a5ffdc5679
chaeyeon41 <rtfgvb098@naver.com>
2023-08-06 23:00:02 +0900

    Committing local changes before merge

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/Account/LoginPage";
 import MainPage from "./pages/Main/MainPage";
 import CategoryPage from "./pages/Category/CategoryPage";
+import ProductPage from "./pages/Product/ProductPage";
 import MyPage from "./pages/MyPage/MyPage";
 import GroupPage from "./pages/Group/GroupPage";
 import GroupAddPage from "./pages/Group/GroupAddPage";
@@ -19,6 +20,7 @@ function App() {
           <Route path="/" element={<LoginPage />}></Route>
           <Route path="/main" element={<MainPage />}></Route>
           <Route path="/category" element={<CategoryPage />}></Route>
+          <Route path="/search" element={<ProductPage />}></Route>
           <Route path="/mypage" element={<MyPage />}></Route>
           <Route path="/group" element={<GroupPage />}></Route>
           <Route path="/group/add" element={<GroupAddPage />}></Route>
--- src/pages/Product/ProductPage.jsx
+++ src/pages/Product/ProductPage.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import "../../styles/Product/ProductPage.css"
+import SearchBar from "../../components/Product/SearchBar";
+import Product from "../../components/Product/Product";
+import Footer from "../../components/Footer/Footer";
 
 const ProductPage = () => {
     return (
-        <div>
-            
+        <div className='ProductPage_container'>
+            <SearchBar></SearchBar>
+            <div className='ProductPage_top'>
+                <div className='ProductPaeg_top_left'>
+                    <div>제철과일</div>
+                    <img src='./img/imgProduct/item_icon.png' alt='상품아이콘'></img>
+                </div>
+                <div className='ProductPage_top_right'>정렬하기</div>
+            </div>
+            <Product></Product>
+            <Product></Product>
+            <Product></Product>
+            <Footer></Footer>
         </div>
     );
 };
