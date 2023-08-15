46bac60a57fe4cd108335178126202f32c582b4d
JaeHyunGround <gothddlek@naver.com>
2023-08-15 17:50:54 +0900

    Fix : CategoryPage 수정
    - 카테고리 페이지에서 카테고리 항목을 누르면 검색 페이지로 넘어가는 로직을 수정했습니다.

--- src/components/Category/CategoryItem.jsx
+++ src/components/Category/CategoryItem.jsx
@@ -3,7 +3,7 @@ import "../../styles/Category/CategoryItem.css"
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const CategoryItem = ({category_id, PROXY}) => {
+const CategoryItem = ({category_name, category_id, PROXY}) => {
 
     const navigate = useNavigate()
 
@@ -16,8 +16,12 @@ const CategoryItem = ({category_id, PROXY}) => {
       ]
     })
 
-    const onProductList = (e) => {
-      navigate(`/search/${e.currentTarget.id}`)
+    const onProductListSub = (e) => {
+      navigate(`/search?subcategory=${e.currentTarget.id}&name=${encodeURIComponent(e.currentTarget.className)}`)
+    }
+
+    const onProductListCategory = (e) => {
+      navigate(`/search?category=${e.currentTarget.id}&name=${encodeURIComponent(e.currentTarget.className)}`)
     }
 
     useEffect(() =>{
@@ -25,14 +29,19 @@ const CategoryItem = ({category_id, PROXY}) => {
       .then((res) => setCategoryItem(res.data))
       .catch((err) => console.log(err))
     }, [])
+
+    // console.log(categoryItem)
+    // console.log(encodeURIComponent("닭/오리/양")) => 닭%2F오리%2F양
+    // console.log(decodeURIComponent("닭%2F오리%2F양")) => 닭/오리/양
     
 
     return (
         <div className='categoryItem_container'>
+          <div className='categoryItem'><span className={category_name} id={category_id} onClick={onProductListCategory}>- 전체</span></div>
             {categoryItem.subcategories.map((item, idx) => 
-                <div className='categoryItem' key={idx}>
-                    <span id={item.id} onClick={onProductList}>- {item.name}</span>
-                </div>
+                  <div className='categoryItem'>
+                    <span className={item.name} id={item.id} onClick={onProductListSub}>- {item.name}</span>
+                  </div>
             )}
         </div>
     );
