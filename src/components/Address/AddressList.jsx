9eed8d15b5beddbb4b67584379f2f4758159fef3
Seol_JY <wlsdud5654@gmail.com>
2023-08-17 05:39:43 +0900

    Refactor: 주소 추가 관련 리펙토링

--- src/components/Address/AddressList.jsx
+++ src/components/Address/AddressList.jsx
@@ -3,12 +3,18 @@ import AddressItem from './AddressItem';
 import "../../styles/Address/AddressList.css";
 
 const AddressList = ({addressList, PROXY, setFixNum}) => {
+    addressList.delivery_address.sort((a, b) => {
+        if (a.is_default && !b.is_default) {
+            return -1; // a가 b보다 앞으로 오도록 정렬
+        } else if (!a.is_default && b.is_default) {
+            return 1; // b가 a보다 앞으로 오도록 정렬
+        }
+        return 0; // 순서 변경이 필요하지 않음
+    });
+    
     return (
+        addressList &&
         <div className='AddressList_container'>
-            <div className='AddressList_header'>
-                <p>나의 배송지</p>
-                <img src='./img/imgAddress/AddressIcon.svg' alt='나의 배송지'/>
-            </div>
             {addressList.delivery_address.map((item, key) => <AddressItem PROXY={PROXY} addressList={addressList} setFixNum={setFixNum} key={key} item={item}/>)}  
         </div>
     );
